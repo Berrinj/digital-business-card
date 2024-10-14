f225dfca19ec7f5cdc703efd3b63540d6ee5cc5a
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-10-14 23:36:37 +0200

    importing icons and profile image to components

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,7 +1,9 @@
+import profilePic from '../assets/profilbilde.png';
+
 export default function ProfilePicture() {
     return (
         <div className="profile-picture">
-            <img src="/src/assets/profilbilde.png" alt="Profile Picture" className="profile-picture-img" />
+            <img src={profilePic} alt="Profile Picture" className="profile-picture-img" />
         </div>
     )
 }
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,10 +1,16 @@
+import xIcon from '../assets/x-icon.svg';
+import facebookIcon from '../assets/facebook-icon.svg';
+import instagramIcon from '../assets/instagram-icon.svg';
+import githubIcon from '../assets/github-icon.svg';
+
+
 export default function SocialMedia() {
     return(
         <div className="social-media">
-            <a href="https://www.x.com/martelilleberre" target="_blank"><img className="social-media--icon" src="\src\assets\x-icon.svg"></img></a>
-            <a href="https://www.facebook.com/marte.lilleberre" target="_blank"><img className="social-media--icon" src="\src\assets\facebook-icon.svg"></img></a>
-            <a href="https://www.instagram.com/berremarte" target="_blank"><img className="social-media--icon" src="\src\assets\instagram-icon.svg"></img></a>
-            <a href="https://github.com/Berrinj" target="_blank"><img className="social-media--icon" src="\src\assets\github-icon.svg"></img></a>
+            <a href="https://www.x.com/martelilleberre" target="_blank"><img className="social-media--icon" src={xIcon}></img></a>
+            <a href="https://www.facebook.com/marte.lilleberre" target="_blank"><img className="social-media--icon" src={facebookIcon}></img></a>
+            <a href="https://www.instagram.com/berremarte" target="_blank"><img className="social-media--icon" src={instagramIcon}></img></a>
+            <a href="https://github.com/Berrinj" target="_blank"><img className="social-media--icon" src={githubIcon}></img></a>
         </div>
     )
 }

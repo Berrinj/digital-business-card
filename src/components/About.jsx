a629d5300ddb94fa077dcdef80aaeefdd7aa0bf6
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-10-15 00:13:15 +0200

    typo fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="about">
             <h2 className="about-title">About</h2>
-            <p className="about-text">Mother of two, working in a kindergarten where the nature is our playground, and studying full-time online at Noroff <small>School of Technology and Digital Media</small>. Graduating Fronend Developement in March 2025.</p>
+            <p className="about-text">Mother of two, working in a kindergarten where the nature is our playground, and studying full-time online at Noroff <small>School of Technology and Digital Media</small>. Graduating Frontend Development in March 2025.</p>
         </div>
     )
 }
